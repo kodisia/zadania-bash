64ce9969499d4cd27d34e7e5fcb8a03f8a7e9d46
kordi <kordisia@outlook.com>
2021-09-16 17:10:21 +0200

    dokonczenie zadan

--- 05_organise_again.sh
+++ 05_organise_again.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-mkdir -p "Immersion week"/{"Day 0","Day 1","Day 3","Day 4"}
+mkdir -p "Immersion week"/{"Day 0","Day 1","Day 2","Day 3","Day 4"}
+cd "Immersion week"
+for ((n=0; n<5; n++))
+do 
+	cd "Day $n" 
+	touch learnings.md 
+	cd .. 
+done
